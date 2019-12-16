faaec444e8e87d58b19d6e07a000064bbb1677bb
Alejandro Cordoba Boder <acordobab@nycumw-nwx0260.na.corp.ipgnetwork.com>
2019-12-16 09:28:34 -0500

    working on it

--- homework6/Question2/medianMaintance.py
+++ homework6/Question2/medianMaintance.py
@@ -3,10 +3,22 @@ import heapq
 minHeap = []
 maxHeap = []
 
+def rebalance():
+    if(len(minHeap) > len(maxHeap) + 1 ):
+        while(len(minHeap) != len(maxHeap)):
+            num = abs(heapq.heappop(minHeap))
+            heapq.heappush(maxHeap, num)
+    elif(len(maxHeap) > len(minHeap) + 1):
+        while(len(maxHeap) != len(minHeap)):
+            num = heapq.heappop(maxHeap)
+            heapq.heappush(minHeap, num)
+
 def main():
     with open('Median.txt') as inputfile:
         num = int(inputfile.readline()) # dont know if I need to calculate when I only have one element
         heapq.heappush(minHeap, -num)
+        round = 1
+        sum = num
 
         for line in inputfile:
             num = int(line)
@@ -20,6 +32,16 @@ def main():
                 #dont forget to balanceate the heaps
                heapq.heappush(maxHeap, num) 
 
-    print(len(maxHeap), len(minHeap))
+            round += 1
+            if (round % 2 == 0):
+                rebalance()
+                sum += abs(minHeap[0])
+            else:
+                if(len(minHeap) > len(maxHeap)):
+                    sum += abs(minHeap[0])
+                else:
+                    sum += maxHeap[0]
+
+    print(sum % 10000)
 
 main()
