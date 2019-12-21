8fcd51daf42fc7f6ad0bc9297b01b5e29fb0ca7c
Alejandro Cordoba Boder <acordobab@acordobab.local>
2019-12-20 21:35:36 -0500

    Still have 4 more tries :(

--- homework6/Question2/medianMaintance.py
+++ homework6/Question2/medianMaintance.py
@@ -4,44 +4,43 @@ minHeap = []
 maxHeap = []
 
 def rebalance():
-    if(len(minHeap) > len(maxHeap) + 1 ):
+    if((len(minHeap) - len(maxHeap)) >= 2 ):
         num = abs(heapq.heappop(minHeap))
         heapq.heappush(maxHeap, num)
-    elif(len(maxHeap) > len(minHeap) + 1):
+    elif((len(maxHeap) - len(minHeap)) >=  2):
         num = heapq.heappop(maxHeap)
-        heapq.heappush(minHeap, num)
+        heapq.heappush(minHeap, -num)
+
+def addNumber(num):
+    if (len(minHeap) == 0 or num < abs(minHeap[0])):
+        heapq.heappush(minHeap, -num)
+    else:
+        heapq.heappush(maxHeap, num)
+
+def getMedian():
+    biggerHeap = minHeap if (len(minHeap) > len(maxHeap)) else maxHeap
+    smallerHeap = maxHeap if (len(minHeap) > len(maxHeap)) else minHeap
+
+    if(len(biggerHeap) == len(smallerHeap)):
+        return abs(smallerHeap[0])
+    else:
+        return abs(biggerHeap[0])
 
 def main():
-    with open('MedianSmall.txt') as inputfile:
+    sum = 0
+    with open('Median.txt') as inputfile:
         num = int(inputfile.readline()) # dont know if I need to calculate when I only have one element
-        heapq.heappush(minHeap, -num)
-        round = 1
-        sum = num
-
         for line in inputfile:
             num = int(line)
-            if (num < abs(minHeap[0])): 
-                heapq.heappush(minHeap, -num)
-            elif(maxHeap and (num > maxHeap[0])):
-               heapq.heappush(maxHeap, num) 
-            elif(len(maxHeap) > len(minHeap)):
-                heapq.heappush(minHeap, -num)
-            else:
-                 #dont forget to balanceate the heaps
-               heapq.heappush(maxHeap, num) 
+            addNumber(num)
+            rebalance()
+            sum += getMedian()
 
-            round += 1
-            if (round % 2 == 0):
-                rebalance()
-            if(len(minHeap) > len(maxHeap)):
-                sum += abs(minHeap[0])
-            else:
-                sum += maxHeap[0]
-
+    print (sum)
     print(sum % 10000)
     print(len(minHeap), len(maxHeap))
 
 main()
 
-# tested answer 1414, 5083, 5129, 6596
+# tested answer 1414, 5083, 5129, 6596, 324, 4882
 # hacer el naive aproach e ir comparando el resultado
